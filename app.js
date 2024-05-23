baf17c62cde57a7ebd567065f5cde15bd0c1e437
Miyabi0213 <gachagachamasako@gmail.com>
2024-05-23 14:21:07 +0900

    Added notes.js and updated app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var helloRouter = require('./routes/hello'); // Add this line
-
+var notesRouter = require('./routes/notes');
 
 var app = express();
 
@@ -26,7 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 app.use('/hello', helloRouter); // Add this line
-
+app.use('/notes', notesRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
